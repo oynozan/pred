4c064eb45c7d225e7576be84b906799f8985ef45
Ozan Andaç <oynozan@hotmail.com>
2026-04-04 12:46:33 +0200

    homepage modifications

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import WalletProvider from "@/components/providers/wallet";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 
@@ -22,9 +23,11 @@ export default function RootLayout({
     return (
         <html lang="en" className={`${manrope.variable} dark h-full antialiased`}>
             <body className="min-h-full flex flex-col">
-                <Navbar />
-                <main className="flex-1 min-h-0 flex flex-col">{children}</main>
-                <Footer />
+                <WalletProvider>
+                    <Navbar />
+                    <main className="flex-1 min-h-0 flex flex-col">{children}</main>
+                    <Footer />
+                </WalletProvider>
             </body>
         </html>
     );
